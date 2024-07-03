c31803ba87670a97b0c4f7eb0a0cd1882cd35cef
Your Name <victorraffaele@gmail.com>
2024-07-03 13:08:44 -0300

    fix: how customers class receive orders info

--- src/customers_info.py
+++ src/customers_info.py
@@ -3,14 +3,15 @@ import pandas as pd
 
 
 class CustomerInfo(Base):
-    def __init__(self, customers: dict):
+    def __init__(self, customers: dict, orders: dict):
         self.__customers = customers
+        self.__orders = orders
 
     def region(self) -> object:
         countrys = super().region(self.__customers)
         return countrys
 
-    def n_purchases(self, df_orders: list[object], inverse=True) -> list[object]:
-        merged_orders = pd.merge(df_orders, self.__customers)
+    def n_purchases(self, inverse=True) -> list[object]:
+        merged_orders = pd.merge(self.__orders, self.__customers)
         n_purchases = super().n_sales(merged_orders, 'customer_id', inverse)
         return n_purchases
